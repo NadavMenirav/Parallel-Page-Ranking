f77f332911967d909c20f6c8b405c237937b05bd
Nadav Menirav <nmenirav@gmail.com>
2025-12-08 19:05:53 +0200

    (Feat) starting the PageRank function

---
+++ pagerank.c
@@ -0,0 +1,14 @@
+#include "graph.h"
+
+#ifndef D
+#define D 0.15 // The jumping factor. The probability the user will stop surfing and make a new search
+#endif
+
+
+void PageRank(Graph *g, int n, float* rank); // result will be returned to rank array
+
+
+// This function will calculate the PageRank score of each node in the graph (see README)
+void PageRank(Graph *g, int n, float* rank) {
+
+}
